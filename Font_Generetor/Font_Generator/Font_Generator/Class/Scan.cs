3724bde2187fccc5f80d3e29ada6c229b10f523c
Kirill <triniti2255@mail.ru>
2017-03-20 17:34:04 +0300

    Add exception for Scan.cs

--- Font_Generetor/Font_Generator/Font_Generator/Class/Scan.cs
+++ Font_Generetor/Font_Generator/Font_Generator/Class/Scan.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using asprise_imaging_api;
 using System.Drawing;
+using System.Windows.Forms;
 
 
 namespace Font_Generator.Class
@@ -15,9 +16,17 @@ namespace Font_Generator.Class
 
         public Scan()
         {
-            Result result = new AspriseImaging().Scan(new Request().AddOutputItem(new RequestOutputItem(AspriseImaging.OUTPUT_RETURN_BASE64, AspriseImaging.FORMAT_JPG))// Output type and image format
-            , "default", true, true); // "select" a scanner or use "default" scanner without selection
-            this.img = result.GetImage(0);
+            try
+            {
+                Result result = new AspriseImaging().Scan(new Request().AddOutputItem(new RequestOutputItem(AspriseImaging.OUTPUT_RETURN_BASE64, AspriseImaging.FORMAT_JPG))// Output type and image format
+              , "default", true, true); // "select" a scanner or use "default" scanner without selection
+                this.img = result.GetImage(0);
+            }
+            catch (asprise_imaging_api.ImagingException)
+            {
+                DialogResult rezult = MessageBox.Show("Сканер не обнаружен.",
+                "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         public Bitmap GetImg()
